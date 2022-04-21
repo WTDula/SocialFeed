004da7e7687cd7ecb5ec37544ab10e18125d04bf
WTDula <wtdula@gmail.com>
2022-04-21 13:50:47 -0500

    added functionality to likedislike component, tested render and each click situation

--- src/Components/LikeDislike/LikeDislike.jsx
+++ src/Components/LikeDislike/LikeDislike.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import "./LikeDislike.css"
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
@@ -5,12 +6,33 @@ import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 
 const LikeDislike = (props) => {
 
-    
+    const [likeButtonClass, setLikeButtonClass] = useState("gray")
+    const [dislikeButtonClass, setDislikeButtonClass] = useState("gray")
+
+    const handleLikeClick = () => {
+        if(likeButtonClass === "gray"){
+            setLikeButtonClass("green")
+            setDislikeButtonClass("gray")
+        }
+        else{
+            setLikeButtonClass("gray")
+        }
+    }
+
+    const handleDislikeClick = () => {
+        if(dislikeButtonClass === "gray"){
+            setDislikeButtonClass("red")
+            setLikeButtonClass("gray")
+        }
+        else{
+            setDislikeButtonClass("gray")
+        }
+    }
 
     return (
         <div>
-            <button><ThumbUpIcon /></button>
-            <button><ThumbDownIcon /></button>
+            <button className={likeButtonClass} onClick={handleLikeClick}><ThumbUpIcon /></button>
+            <button className={dislikeButtonClass} onClick={handleDislikeClick}><ThumbDownIcon /></button>
         </div>
      );
 }
